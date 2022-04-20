d09be7ed9815fa382d53805ed4e0e5bf3a04cc0e
lukaszwojcik89 <wojcik.l@tlen.pl>
2022-04-20 20:49:13 +0200

    adding list of supported browsers

--- src/index.js
+++ src/index.js
@@ -1,27 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {key: 1}, props.name),
-    React.createElement("h3", {key: 2}, props.animal),
-    React.createElement("h3", {key: 3}, props.breed),
-  ]);
-};
-const App = () => {
-  return React.createElement("div", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    })
-};
+import App from "./App";
 
-const container = document.getElementById("app");
-const root = ReactDOM.createRoot(container);
+// const container = document.getElementById("app");
+// const root = ReactDOM.createRoot(container);
+const root = ReactDOM.createRoot(document.getElementById("app"));
 root.render(React.createElement(App));
