4f5fcf00d9b976721a934e461775030bcf096d51
Łukasz Wójcik <wojcik.l@tlen.pl>
2022-07-05 20:07:13 +0200

    Adding tests for Pets

--- src/__tests__/Pet.test.js
+++ src/__tests__/Pet.test.js
@@ -2,13 +2,30 @@
  * @jest-environment jsdom
  */
 
-import { expect, test } from '@jest/globals';
-import { render } from '@testing-library/react';
-import Pet from '../components/Pet.jsx'
+import { expect, test } from "@jest/globals";
+import { render } from "@testing-library/react";
+import { StaticRouter } from "react-router-dom/server";
+import Pet from "../components/Pet.jsx";
 
 test("displays a default thumbnail", async () => {
-    const pet = render(<Pet />);
+    const pet = render(
+        <StaticRouter>
+            <Pet />
+        </StaticRouter>
+    );
 
-    const perThumbnail = await pet.findByTestId('thumbnail');
-    expect(perThumbnail.src).toContain('none.jpg');
-})
+    const petThumbnail = await pet.findByTestId("thumbnail");
+    expect(petThumbnail.src).toContain("none.jpg");
+});
+
+test ("displays a non-default thumbnail", async () => {
+    const pet = render(
+        <StaticRouter>
+            <Pet images={["1.jpg", "2.png", "3.webm"]} />
+        </StaticRouter>
+    );
+
+    const petThumbnail = await pet.findByTestId("thumbnail");
+    expect(petThumbnail.src).toContain("1.jpg");
+
+});
--- src/components/Pet.jsx
+++ src/components/Pet.jsx
@@ -1,19 +1,21 @@
+import { Link } from "react-router-dom";
+
 function Pet({ name, animal, breed, images, location, id }) {
     let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
-    if (images.length) {
+    if (Array.isArray(images) && images.length) {
         hero = images[0];
     }
 
     return (
-        <a href={`/details/${id}`} className="pet">
+        <Link to={`/details/${id}`} className="pet">
             <div className="image-container">
-                <img src={hero} alt={name} />
+                <img src={hero} alt={name} data-testid="thumbnail" />
             </div>
             <div className="info">
                 <h1>{name}</h1>
                 <h2>{`${animal} — ${breed} — ${location}`}</h2>
             </div>
-        </a>
+        </Link>
     );
 }
 
