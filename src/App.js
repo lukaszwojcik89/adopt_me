56eae00b819b21d9caa0c9b13b56ed607b1e4308
Łukasz Wójcik <wojcik.l@tlen.pl>
2022-07-07 14:11:57 +0200

    QuickFix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
-import SearchParams from "./components/SearchParams.jsx";
-import Details from "./components/Details.jsx";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 import "./App.css";
+import SearchParams from "./components/SearchParams.jsx";
+import Details from "./components/Details.jsx";
+import ThemeContext from "./components/ThemeContext.jsx";
+import { useState } from "react";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <header>
-                <Link to="/">
-                    <h1>Adopt Me</h1>
-                </Link>
-            </header>
+    const theme = useState("green");
 
-            <Routes>
-                <Route path="/" element={<SearchParams />} />
-                <Route path="/details/:id" element={<Details />} />
-            </Routes>
-        </BrowserRouter>
+    return (
+        <ThemeContext.Provider value={theme}>
+            <BrowserRouter>
+                <header>
+                    <Link to="/">
+                        <h1>Adopt Me!</h1>
+                    </Link>
+                </header>
+
+                <Routes>
+                    <Route path="/" element={<SearchParams />} />
+                    <Route path="/details/:id" element={<Details />} />
+                </Routes>
+            </BrowserRouter>
+        </ThemeContext.Provider>
     );
 }
 
